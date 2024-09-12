8402d7aaa42734b731a2f170773aeb9b89560024
Jainam <jainam2210patel@gmail.com>
2024-09-12 23:13:01 +0530

    Save cart in localstorage.

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,9 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2
-},{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:2
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function saveToStorage(){
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productid) {
 
@@ -24,6 +23,8 @@ export function addToCart(productid) {
             quantity: 1
         });
   }
+
+  saveToStorage();
   
 }
 
@@ -39,4 +40,6 @@ export function removeFromCart(productid){
     });
 
     cart=newCart;
+
+    saveToStorage();
 }
