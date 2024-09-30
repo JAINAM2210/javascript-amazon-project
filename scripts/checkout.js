5766694b10f2cf996b515c3ad18542a7ce9fb159
Jainam <jainam2210patel@gmail.com>
2024-10-01 01:54:26 +0530

    Callbacks and promises.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,34 @@ import {loadProducts} from '../data/products.js';
 import {renderOrderPage} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummmary.js';
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) =>{
+        loadProducts(()=>{
+            resolve('pass values here for next promise');
+        });
+    }), new Promise((resolve) =>{
+        loadProducts(()=>{
+            resolve('hi');
+        });
+    })  
+]).then((allValues)=>{ //incase of .all, allValues will be a array and store all the passed values from previous promises
     renderOrderPage();
     renderPaymentSummary();
+    console.log(allValues);
 });
+
+/*
+new Promise((resolve) =>{
+    loadProducts(()=>{
+        resolve();
+    });
+}).then(()=>{ 
+    renderOrderPage();
+    renderPaymentSummary();
+    
+});
+*/
+
+// loadProducts(() => {
+   
+// });
